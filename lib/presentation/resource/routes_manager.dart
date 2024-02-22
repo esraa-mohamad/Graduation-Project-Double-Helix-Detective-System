9a82a875861885b5e5ac89ef56b53643e1e26617
midohamza22 <91686000+midoHaz@users.noreply.github.com>
2024-02-22 20:24:36 +0200

    add Routes manager class to routes_manager.dart

---
+++ lib/presentation/resource/routes_manager.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class RoutesManager{
+  static const String splashRoute='/';
+  static const String loginRoute='/login';
+  static const String servicesPresentedRoute='/servicesPresented';
+  static const String paternityFormRoute='/paternityForm';
+  static const String paternityResultRoute='/paternityResult';
+  static const String identificationFormRoute='/identificationForm';
+  static const String identificationResultRoute='/identificationResult';
+  static const String missingPersonFormRoute='/missingPersonForm';
+  static const String missingPersonResultRoute='/missingPersonResult';
+  static const String populationRoute='/population';
+}
+
+class RouteGenerator{
+  // static Route<dynamic>getRoute(RouteSettings settings){
+  //   switch(settings.name){
+  //     case RoutesManager.splashRoute:
+  //       return MaterialPageRoute(builder: (_)=>)
+  //   }
+  // }
+}
