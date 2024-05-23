9e837733c8475d5ac33ed3f83a240a08dc153f33
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2024-05-24 01:18:53 +0300

    resize desktop windows and change icon of app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'app/app.dart';
 import 'app/di.dart';
@@ -6,6 +9,12 @@ import 'app/di.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await initAppModule();
+  await windowManager.ensureInitialized();
+  if (Platform.isWindows) {
+    WindowManager.instance.setTitle('Double Helix Detective System');
+    //WindowManager.instance.setMaximumSize(const Size(1600, 800));
+    WindowManager.instance.setMinimumSize(const Size(1200, 700));
+  }
   runApp(MyApp());
 }
 
